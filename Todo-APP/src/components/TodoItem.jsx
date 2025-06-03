5876003f9c09866381fff80d198ac5e4078c5d50
Saeed Ali <saeedaliofficial77@gmail.com>
2025-06-04 02:58:20 +0500

    Marking Todo Items As Complete

--- Todo-APP/src/components/TodoItem.jsx
+++ Todo-APP/src/components/TodoItem.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import './Todo.css';
 
-const TodoItem = ({item,todos,setTodos}) => {
-  const handleDelete = (item) =>{
-    console.log("clicked",item);
-    setTodos( todos.filter((todo)=> todo !== item ))
-   
+const TodoItem = ({item, todos, setTodos}) => {
+  const handleDelete = (item) => {
+    console.log("clicked", item);
+    setTodos(todos.filter((todo) => todo !== item));
   }
+
+  const handleCheck = (item) => {
+    const newArray = todos.map((todo) => 
+      todo === item ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray)
+  
+    
+  }
+
   return (
-   
     <div className="todo-item">
-      <input type="checkbox" className="todo-checkbox" />
-      <span className="todo-text">{item.name  }</span>
-      <button onClick={()=> handleDelete(item)} className="delete-button">×</button>
+      <input 
+        type="checkbox" 
+        className="todo-checkbox" 
+        checked={item.done}
+        onChange={() => handleCheck(item)}
+      />
+      <span className={`todo-text ${item.done ? 'completed' : ''}`}>{item.name}</span>
+      <button onClick={() => handleDelete(item)} className="delete-button">×</button>
     </div>
   )
 }
