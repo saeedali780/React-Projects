20fddb0ca555b82efc04c20704d287b124ca6247
Saeed Ali <saeedaliofficial77@gmail.com>
2025-08-11 04:14:48 +0500

    header designed

--- Movie-App/src/App.jsx
+++ Movie-App/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import Search from "./components/Search"
+
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <main>
       <div className="pattern">
         <div className="wrapper">
           <header>
+            <img src="./hero.png" alt="logo" className="logo" />
             <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hustle</h1>
           </header>
-          <p>Search</p>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}  />
+          <h1 className="text-white">{searchTerm}</h1>
         </div>
       </div>
     </main>
