a0056c61f11563759f4e196da983a4aad0406730
Saeed Ali <saeedaliofficial77@gmail.com>
2025-08-11 06:11:08 +0500

    Movie Card component created

---
+++ Movie-App/src/components/MovieCard.jsx
@@ -0,0 +1,26 @@
+
+const MovieCard = (props) => {
+    const {title,poster_path,vote_average,original_language ,release_date}= props.movie;
+  return (
+    <div className="movie-card">
+        <img 
+        src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title} />
+       <div className="mt-4">
+        <h3>{title}</h3>
+
+        <div className="content">
+            <div className="rating">
+                <img src="./star.svg" alt="rating" />
+                <span>{vote_average? vote_average.toFixed(1) : "N/A"}</span>
+            </div>
+            <span>•</span>
+            <p className="lang">{original_language}</p>
+            <span>•</span>
+            <p className="year">{release_date ? release_date.split('-')[0]: 'N/A' }</p>
+        </div>
+       </div>
+    </div>
+  )
+}
+
+export default MovieCard
